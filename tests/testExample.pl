1b54128cb0e0619e5dd1676b23a520a838190733
Will Budic <vilijanac@gmail.com>
2022-11-17 16:03:22 +1100

    Added example.cnf and test file for it.

--- tests/testExample.pl
+++ tests/testExample.pl
@@ -3,10 +3,7 @@ use warnings; use strict;
 use Syntax::Keyword::Try;
 
 use lib "./tests";
-#use lib "./system/modules";
-use lib "/home/will/dev/PerlCNF/system/modules";
-
-
+use lib "./system/modules";
 
 require TestManager;
 require CNFParser;
