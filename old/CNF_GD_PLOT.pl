4f0d42c30e3c7401e9363b5849e2f7f0fe6a9c7d
Will Budic <vilijanac@gmail.com>
2023-01-12 16:42:08 +1100

    Revisited with new CNFSQL.

--- old/CNF_GD_PLOT.pl
+++ old/CNF_GD_PLOT.pl
@@ -1,27 +1,28 @@
 #!/usr/bin/perl
 
-
+#use strict;
 use warnings; 
 use Try::Tiny;
 use Exception::Class ('CNFParserException');
-use DBI;  use GD::Graph::lines;
+use DBI;  
+use GD::Graph::lines;
 
 #DEFAULT SETTINGS HERE!
 #LanguageServer doesn't like -> $ENV{'PWD'} settings.json should not be set for it withn an pwd.
 #use lib "system/modules/";
-use lib "./system/modules/";
+use lib "system/modules";
 require CNFParser;
 
-my $cnf = CNFParser->new('databaseBitcoinPlot.cnf', {DO_enabled=>1});
-my $DSN = CNFParser::anon('DBI_SOURCE');
+my $cnf = CNFParser->new('old/databaseBitcoinPlot.cnf', {DO_enabled=>1,ANONS_ARE_PUBLIC=>1}); #Since v.2.6 ANONS_ARE_PUBLIC=>0 is assumed if not specified.
+my $DSN = CNFParser::anon('DBI_SOURCE'); #<- Global static access we use, as it is available, it is same as: $cnf->anon('DBI_SOURCE');
 my $alin= $cnf->anon('AUTO_LOGIN');
 my ($u,$p) = split '/', $alin;
 my $db  =  DBI->connect($DSN, $u, $p, {AutoCommit => 1, RaiseError => 1, PrintError => 0, show_trace=>1});
 
 #my $sql = $cnf->anons('SEL_BITCOIN_30_DAY_RANGE');
-my $sql = $cnf->anon('SEL_BITCOIN_3_MONTH_RANGE');
+my $sql = $cnf->SQL()->{'SEL_BITCOIN_3_MONTH_RANGE'};
 print "$0 SQL -> $sql\n";
-my $stm = $cnf-> selectRecords($db, $sql);
+my $stm = CNFParser::SQL() -> selectRecords($db, $sql);
 
 my @DAT=();my @MAX=();my @MIN=();my @AVG=();
 my $c=0;
@@ -32,11 +33,11 @@ while( my @a = $stm->fetchrow_array() ){
 
 my @data = ([@DAT], [@MAX], [@AVG], [@MIN]);
 
-my @dim = @{$cnf->collection('@DIM_SET_BITCOIN')};
+my @dim = $cnf->collection('@DIM_SET_BITCOIN');
 my $graph = GD::Graph::lines->new(@dim);
-my %hsh = %{$cnf->collection('%HSH_SET_BITCOIN_LINE_PLOT_RANGE')};
+my %hsh = $cnf->collection('%HSH_SET_BITCOIN_LINE_PLOT_RANGE');
 $graph->set(%hsh);
-$graph->set_legend_font(GD::gdFontTiny);
+$graph->set_legend_font(GD::Graph::gdFontTiny);
 $graph->set_legend('Max','Avg', 'Min');
 my $gd = $graph->plot( \@data ) or die "Error encountered ploting graph: $!";
 
@@ -45,6 +46,7 @@ open $OUT, ">","./BitcoinCurrentLast30Days.png" or die "Couldn't open for output
 binmode($OUT);
 print $OUT $gd->png();
 close $OUT;
+
 
 
 
